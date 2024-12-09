4aeabf23408af40f677c874209ae94a3a5077bd1
Treevyy <aarontrevormoore@gmail.com>
2024-12-09 17:59:55 -0600

    Added start screen and start game button.

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+document.getElementById('start-button').addEventListener('click', function() {
+    document.getElementById('game-title').style.display = 'none';
+    this.style.display = 'none';
+    alert('Game Started!');
+});
