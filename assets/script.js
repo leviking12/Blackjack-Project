b724ba6348199bdacc2b623db6c91174bf3cf051
Joseph Llanos <josephllanos0220@gmail.com>
2024-12-12 15:04:44 -0500

    Start page no longer shows game content and modal button is hidden

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,47 @@
-//player hand that will recieve values from 1-13
+// Start page functionality
+document.getElementById('start-button').addEventListener('click', function() {
+    
+    // Hide start page elements
+    document.getElementById('game-title').style.display = 'none';
+    this.style.display = 'none';
+        
+    // Show the game page upon clicking start button
+    document.getElementById('game-page').style.display = 'block';
+        
+    // Start the game
+    startGame();
+    });
+
+// Player hand that will recieve values from 1-13
 var playerHand = [];
 
-//dealer hand that will recieve values from 1-13
+// Dealer hand that will recieve values from 1-13
 var dealerHand = [];
 
-//all of the cards in the deck (11,12,13 all equal 10, and 1 equals 1 or 10)
+// All of the cards in the deck (11,12,13 all equal 10, and 1 equals 1 or 10)
 const cards = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 
-//Gives the player and dealer 2 cards
+// Gives the player and dealer 2 cards
 function startGame() {
 
     checkHand();
 }
 
-//Gives the player 1 card
+// Gives the player 1 card
 function draw() {
 
     checkHand();
 }
 
-//Give the dealer cards until they are greater than or equal to 17
+// Give the dealer cards until they are greater than or equal to 17
 function stand() {
 
     endGame();
 }
 
-//If Player is over 21, then end game.
-//If Player is under 21, continue.
-//if Player is 21, stand.
+// If Player is over 21, then end game.
+// If Player is under 21, continue.
+// If Player is 21, stand.
 function checkHand() {
     var handTotal = 0;
     var aceCount = 0;
@@ -55,7 +69,7 @@ function checkHand() {
         stand();
 }
 
-//end the game and open the modal
+// End the game and open the modal
 function endGame() {
 
 }
